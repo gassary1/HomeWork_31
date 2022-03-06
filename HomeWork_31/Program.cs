c92530dcbcff6493ef6248477de5bca70e5926ff
gassa <gassa@DESKTOP-BAL2EJ4>
2022-03-07 01:05:03 +0300

    Добавьте файлы проекта.

---
+++ HomeWork_31/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HomeWork_31
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
